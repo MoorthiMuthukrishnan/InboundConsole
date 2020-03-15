6ac4e8b610349e7d01ae4dffee105634c0cc9c94
MoorthiMuthukrishnan <62007704+MoorthiMuthukrishnan@users.noreply.github.com>
2020-03-15 21:44:51 +0530

    Added console application

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using InboundEntryConsole.Models;
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace OutboundEntryConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new QueueSystemDBContext())
+            {
+
+                var query = from map in context.MappingDetails
+                            where map.IsProcessed == false
+                            orderby map.PublisherId, map.ConsumerId, map.CreatedDate ascending
+                            select map;
+
+                var consumerId = 0;
+                var publisherID = 0;
+
+                foreach (var q in query)
+                {
+                    if (consumerId != q.ConsumerId && publisherID!= q.PublisherId)
+                    {
+                        context.InboundEntry.Add(new InboundEntry
+                        {
+                            MappingId = q.MappingId,
+                            CreatedDate = DateTime.Now
+                        }
+                        );
+
+                        consumerId = q.ConsumerId;
+                        publisherID = q.PublisherId;
+                    }
+                }
+                context.SaveChanges();
+            }
+        }
+    }
+}
